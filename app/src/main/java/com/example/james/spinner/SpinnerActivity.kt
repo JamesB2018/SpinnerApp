e049ad2ed30922445fdf17e36ccd817072f36ac4
James <jmb47580@ucmo.edu>
2018-06-30 19:21:33 -0500

    Added adapter and created multiple options

--- app/src/main/java/com/example/james/spinner/SpinnerActivity.kt
+++ app/src/main/java/com/example/james/spinner/SpinnerActivity.kt
@@ -2,8 +2,12 @@ package com.example.james.spinner
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import org.w3c.dom.Text
 
 
 class SpinnerActivity : AppCompatActivity() {
@@ -14,6 +18,23 @@ class SpinnerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner)
+
+        option = findViewById(R.id.spinner_option)
+        result = findViewById(R.id.result) as TextView
+
+        val options = arrayOf("Option 1", "Option 2", "Option 3")
+
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+               result.text = "Select and option"
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                result.text = options.get(position)
+            }
+        }
 
     }
 }
